9d5f82cd15bd074efbf9405e9354b63270317417
Max Starikevich <maxim.starikevich@gmail.com>
2021-08-19 21:27:46 +0300

    Removed node id from logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,5 +1,4 @@
 import { createLogger, transports, format } from 'winston';
-import config from './config';
 
 export const logger = createLogger({
   level: 'info',
@@ -9,8 +8,7 @@ export const logger = createLogger({
       JSON.stringify({
         level: data.level,
         timestamp: data.timestamp,
-        message: data.message,
-        pm2NodeId: config.PM2_NODE_ID
+        message: data.message
       })
     )
   ),
