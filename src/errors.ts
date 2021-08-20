e83cb2729d9105353561c94382e30d14272b3e92
Max Starikevich <6186896+max-starikevich@users.noreply.github.com>
2021-08-20 23:26:19 +0300

    Release 1.2.0 (#47)
    * Fixed buildspec
    
    * Added Sentry

--- src/errors.ts
+++ src/errors.ts
@@ -1,17 +1,25 @@
+import * as Sentry from '@sentry/node';
+
 import { logger } from '@/logger';
+import { wait } from '@/utils';
 
 export const handleActionError = (error: Error) => {
   logger.error('❌ Action failed.', error);
+  Sentry.captureException(error);
 };
 
 export const handleStartupError = (error: Error) => {
   logger.error('❌ Startup failed.', error);
-  process.exit(1);
+  Sentry.captureException(error);
+
+  wait(5000).then(() => process.exit(1));
 };
 
 export const handleUnexpectedRejection = (error: any) => {
   logger.error('❌ Unexpected rejection.', error);
-  process.exit(1);
+  Sentry.captureException(error);
+
+  wait(5000).then(() => process.exit(1));
 };
 
 export class HandledError extends Error {}
