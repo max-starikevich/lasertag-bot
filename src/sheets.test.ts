64755d37e150a6c35b8db6ffe9ecc2163d36546b
Max Starikevich <maxim.starikevich@gmail.com>
2021-09-02 23:25:44 +0300

    Fixed sheets test

--- src/sheets.test.ts
+++ src/sheets.test.ts
@@ -4,18 +4,21 @@ import {
 } from 'google-spreadsheet';
 
 import { getPlaceAndTime } from '@/sheets';
+import config from '@/config';
 
 describe('Sheets module', () => {
   describe('getPlaceAndTime()', () => {
     it('should return place and time from the Google Spreadsheet', async () => {
-      const data = [['', '', 'Среда 19:00 Гонолес']];
+      const data: { [key: string]: string } = {
+        [config.PLACE_AND_TIME_CELL]: 'Среда 19:00 Гонолес'
+      };
 
       const sheet = {
         loadCells: async () => {
           // do nothing
         },
-        getCell: (rowIndex: number, columnIndex: number) => ({
-          value: data[rowIndex][columnIndex]
+        getCellByA1: (a1: string) => ({
+          value: data[a1]
         })
       } as unknown as GoogleSpreadsheetWorksheet;
 
@@ -28,7 +31,7 @@ describe('Sheets module', () => {
 
       const placeAndTime = await getPlaceAndTime(document);
 
-      expect(placeAndTime).toEqual(data[0][2]);
+      expect(placeAndTime).toEqual(data[config.PLACE_AND_TIME_CELL]);
     });
   });
 });
