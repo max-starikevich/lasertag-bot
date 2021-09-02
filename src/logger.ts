165a36a90a47276e26049cc2cb19ae1ef983278f
Max Starikevich <maxim.starikevich@gmail.com>
2021-09-02 22:45:22 +0300

    Improved logging

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,19 @@
 import { createLogger, format, transports } from 'winston';
 
-const { combine, timestamp, errors, prettyPrint } = format;
+const { combine, timestamp, errors, printf } = format;
 
 export const logger = createLogger({
-  format: combine(errors({ stack: true }), timestamp(), prettyPrint()),
+  format: combine(
+    errors({ stack: true }),
+    timestamp(),
+    printf((info) =>
+      JSON.stringify({
+        timestamp: info.timestamp,
+        level: info.level,
+        message: info.message,
+        stack: info.stack
+      })
+    )
+  ),
   transports: [new transports.Console()]
 });
