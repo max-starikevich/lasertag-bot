15afa01283a1cc5752e50f7700ab748e27ceecee
Max Starikevich <maxim.starikevich@gmail.com>
2021-09-12 08:54:06 +0300

    Added new player props

--- src/player.ts
+++ src/player.ts
@@ -4,10 +4,13 @@ import { getRandomOneOrZero } from './utils';
 
 export interface Player {
   name: string;
+  combinedName: string;
   count: number;
   rentCount: number;
   comment: string;
   level: number;
+  isQuestionable: boolean;
+  isCompanion: boolean;
 }
 
 export type Teams = [Player[], Player[]];
