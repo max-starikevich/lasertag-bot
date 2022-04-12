22afa441cad2d6a201f312ca75b0bc14b0afe949
Max Starikevich <maxim.starikevich@gmail.com>
2022-04-13 01:18:56 +0300

    Improved logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,20 +1,33 @@
+import { Format } from 'logform'
 import { createLogger, format, transports } from 'winston'
+
+import config from '$/config'
 
 const { combine, timestamp, errors, printf, prettyPrint } = format
 
+const defaultFormats: Format[] = [
+  errors({ stack: true }),
+  timestamp(),
+  printf((info) =>
+    JSON.stringify({
+      level: info.level,
+      message: info.message,
+      stack: info.stack,
+      timestamp: info.timestamp
+    })
+  )
+]
+
+const devFormats: Format[] = [
+  prettyPrint({ colorize: true })
+]
+
 export const logger = createLogger({
   format: combine(
-    errors({ stack: true }),
-    timestamp(),
-    printf((info) =>
-      JSON.stringify({
-        level: info.level,
-        message: info.message,
-        stack: info.stack,
-        timestamp: info.timestamp
-      })
-    ),
-    prettyPrint()
+    ...[
+      ...defaultFormats,
+      ...(config.isLocal ? devFormats : [])
+    ]
   ),
   transports: [new transports.Console()]
 })
