a532e979a62ad350f4a16fb0eb6a9303a83ab28e
Max Starikevich <maxim.starikevich@gmail.com>
2022-04-20 14:47:25 +0300

    Moved webhooks script

--- src/scripts/deploy-webhooks.ts
+++ src/scripts/deploy-webhooks.ts
@@ -4,7 +4,9 @@ dotenv.config({ path: '.env.production' })
 import 'module-alias/register'
 
 import { Telegraf } from 'telegraf'
+import axios from 'axios'
 
+import { version } from '../../package.json'
 import config, { checkEnvironment } from '$/config'
 import { logger } from '$/logger'
 import { BotContext, updateWebhook } from '$/bot'
@@ -12,12 +14,22 @@ import { BotContext, updateWebhook } from '$/bot'
 async function run (): Promise<void> {
   try {
     await checkEnvironment()
-    logger.info(`✅ Environment is OK. APP_ENV: ${config.APP_ENV}`)
 
     const bot = new Telegraf<BotContext>(config.BOT_TOKEN)
+    
     await updateWebhook(bot)
+    
+    logger.info(`✅ Telegram webhook has been updated: ${config.WEBHOOK_BASE}/*`)
 
-    logger.info(`✅ Webhook has been updated: ${config.WEBHOOK_BASE}/*`)
+    const sentryWebhook = process.env.SENTRY_DEPLOY_WEBHOOK
+
+    if (sentryWebhook && new URL(sentryWebhook)) {
+      await axios.post(sentryWebhook, {
+        version
+      })
+
+      logger.info(`✅ Created v${version} release in Sentry`)
+    }
   } catch (e) {
     logger.error(e)
     process.exit(1)
