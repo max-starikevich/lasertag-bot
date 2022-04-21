f46e5f4dbf9e9912fc11f146a88290557a90d087
Max Starikevich <maxim.starikevich@gmail.com>
2022-04-21 20:23:25 +0300

    Removed useless getDateDiffInSeconds

--- src/utils.ts
+++ src/utils.ts
@@ -16,9 +16,3 @@ export const parseJsonSafe = (json: string): any => {
     return null
   }
 }
-
-export const getDateDiffInSeconds = (a: Date, b: Date): number => {
-  const diff = a.getTime() - b.getTime()
-
-  return diff / 1000
-}
