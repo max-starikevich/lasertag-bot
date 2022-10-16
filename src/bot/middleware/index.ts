d4635893acf493d8afaccec500707ce32b99f038
Max Starikevich <maxim.starikevich@gmail.com>
2022-10-17 02:28:55 +0300

    Better error handling. Including Sentry reports

--- src/bot/middleware/index.ts
+++ src/bot/middleware/index.ts
@@ -1,99 +1,84 @@
 import { performance } from 'perf_hooks'
-import { Telegraf } from 'telegraf'
+import { Telegraf, MiddlewareFn, NarrowedContext } from 'telegraf'
+import { Update } from 'typegram'
 
 import config from '$/config'
 import { logger } from '$/logger'
+import { captureException } from '$/errors'
 
 import { GameContext } from '../types'
 import { commands } from '../commands'
 
 export const setBotMiddlewares = (bot: Telegraf<GameContext>): void => {
-  setHomeChatAccess(bot)
-  setLogging(bot)
-  setCommands(bot)
-  setUnknownCommands(bot)
-}
+  bot.on('message', homeChatAccessMiddleware)
+  bot.use(errorLoggingMiddleware)
 
-const setHomeChatAccess = (bot: Telegraf<GameContext>): void => {
-  bot.on('message', async (ctx, next) => {
-    const startMs = performance.now()
-
-    try {
-      await ctx.telegram.getChatMember(config.TELEGRAM_HOME_CHAT_ID, ctx.from.id)
-
-      const endMs = performance.now()
-      const timeElapsedMs = Math.round(endMs - startMs)
-
-      logger.info({
-        update: ctx.update,
-        status: 'ACCESS_OK',
-        timeElapsedMs
-      })
-    } catch (error) {
-      const endMs = performance.now()
-      const timeElapsedMs = Math.round(endMs - startMs)
-
-      logger.error({
-        update: ctx.update,
-        status: 'ACCESS_NOT_OK',
-        error,
-        timeElapsedMs
-      })
-
-      await ctx.reply('⚠️ Нет доступа')
-      return
-    }
-
-    await next()
-  })
-}
-
-const setLogging = (bot: Telegraf<GameContext>): void => {
-  bot.use(async (ctx, next) => {
-    const startMs = performance.now()
-
-    try {
-      await next()
-
-      const endMs = performance.now()
-      const timeElapsedMs = Math.round(endMs - startMs)
-
-      logger.info({
-        update: ctx.update,
-        status: 'COMMAND_OK',
-        timeElapsedMs
-      })
-    } catch (error) {
-      const endMs = performance.now()
-      const timeElapsedMs = Math.round(endMs - startMs)
-
-      logger.error({
-        update: ctx.update,
-        status: 'COMMAND_NOT_OK',
-        error,
-        timeElapsedMs
-      })
-    }
-  })
-}
-
-const setCommands = (bot: Telegraf<GameContext>): void => {
   commands.map((command) =>
-    bot.command('/' + command.name, async (ctx) => {
-      try {
-        await command.handler(ctx)
-      } catch (e) {
-        logger.error(e)
-        void ctx.reply('⚠️ Неожиданная ошибка. Повторите запрос позже.')
-      }
-    })
+    bot.command('/' + command.name, async (ctx) => await command.handler(ctx))
   )
-}
 
-const setUnknownCommands = (bot: Telegraf<GameContext>): void => {
   bot.hears(/^\/[a-z0-9]+$/i, async (ctx) => {
     await ctx.reply(
       '⚠️ Не удалось распознать команду. Используйте меню или команду /help'
     )
   })
+}
+
+const homeChatAccessMiddleware: MiddlewareFn<NarrowedContext<GameContext, Update.MessageUpdate>> = async (ctx, next) => {
+  const startMs = performance.now()
+
+  try {
+    await ctx.telegram.getChatMember(config.TELEGRAM_HOME_CHAT_ID, ctx.from.id)
+    const timeElapsedMs = Math.round(performance.now() - startMs)
+
+    logger.info({
+      update: ctx.update,
+      status: 'ACCESS_OK',
+      timeElapsedMs
+    })
+  } catch (error) {
+    const timeElapsedMs = Math.round(performance.now() - startMs)
+
+    captureException(error)
+
+    logger.warn({
+      update: ctx.update,
+      status: 'ACCESS_NOT_OK',
+      error,
+      timeElapsedMs
+    })
+
+    await ctx.reply('⚠️ Нет доступа')
+    return
+  }
+
+  await next()
+}
+
+const errorLoggingMiddleware: MiddlewareFn<GameContext> = async (ctx, next) => {
+  const startMs = performance.now()
+
+  try {
+    await next()
+    const timeElapsedMs = Math.round(performance.now() - startMs)
+
+    logger.info({
+      update: ctx.update,
+      status: 'COMMAND_OK',
+      timeElapsedMs
+    })
+  } catch (error) {
+    const timeElapsedMs = Math.round(performance.now() - startMs)
+
+    captureException(error)
+
+    logger.error({
+      update: ctx.update,
+      status: 'COMMAND_NOT_OK',
+      error,
+      timeElapsedMs
+    })
+
+    void ctx.reply('⚠️ Неожиданная ошибка. Повторите запрос позже.')
+  }
 }
