78155385fa8c7bb0f1880bb7c012e718d3a28079
Max Starikevich <maxim.starikevich@gmail.com>
2022-10-17 02:28:35 +0300

    Removed useless classes

--- src/bot/errors/SystemError.ts
+++
@@ -1,5 +0,0 @@
-export class SystemError extends Error {
-  constructor (public message: string) {
-    super(message)
-  }
-}
--- src/game/table/errors/NoDocumentError.ts
+++
@@ -1,2 +0,0 @@
-import { TableError } from './TableError'
-export class NoDocumentError extends TableError {}
