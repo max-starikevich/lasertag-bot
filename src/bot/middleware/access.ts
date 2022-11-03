503df946e7adf4de284a21945320c7ff9d014044
Max Starikevich <maxim.starikevich@gmail.com>
2022-11-03 12:50:56 +0300

    Lint fix

--- src/bot/middleware/access.ts
+++ src/bot/middleware/access.ts
@@ -9,9 +9,9 @@ import { GameContext } from '../types'
 export const accessMiddleware: MiddlewareFn<NarrowedContext<GameContext, Update.MessageUpdate>> = async (ctx, next) => {
   try {
     const { status } = await ctx.telegram.getChatMember(config.TELEGRAM_HOME_CHAT_ID, ctx.from.id)
-    
+
     if (!['creator', 'member', 'administrator'].includes(status)) {
-      throw new NoHomeChatAccessError(`This member isn't a member of the group`)
+      throw new NoHomeChatAccessError('This member isn\'t a member of the group')
     }
   } catch (error) {
     if (error instanceof NoHomeChatAccessError) {
