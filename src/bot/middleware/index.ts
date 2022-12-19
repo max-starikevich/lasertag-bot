3ee82bfe65c8401a8a30b210df95586d0f39bf70
Max Starikevich <maxim.starikevich@gmail.com>
2022-12-20 00:04:06 +0300

    Middleware refactor

--- src/bot/middleware/index.ts
+++ src/bot/middleware/index.ts
@@ -1,4 +1,5 @@
-import { Telegraf } from 'telegraf'
+import { Telegraf, MiddlewareFn, NarrowedContext } from 'telegraf'
+import { Update } from 'telegraf/typings/core/types/typegram'
 
 import { GameContext } from '../types'
 import { commands } from '../commands'
@@ -6,9 +7,12 @@ import { commands } from '../commands'
 import { accessMiddleware } from './access'
 import { errorMiddleware } from './error'
 import { loggingMiddleware } from './logging'
+import { analyticsMiddleware } from './analytics'
+
+export type BotMiddleware = MiddlewareFn<NarrowedContext<GameContext, Update.MessageUpdate>>
 
 export const setBotMiddlewares = (bot: Telegraf<GameContext>): void => {
-  bot.on('message', loggingMiddleware, accessMiddleware)
+  bot.on('message', loggingMiddleware, analyticsMiddleware, accessMiddleware)
 
   commands.map((command) => bot.command('/' + command.name, async (ctx) => await command.handler(ctx)))
 
--- src/bot/middleware/logging.ts
+++ src/bot/middleware/logging.ts
@@ -1,11 +1,9 @@
-import { MiddlewareFn } from 'telegraf'
+import { BotMiddleware } from '.'
 import { makeLogger } from '../../logger'
 
-import { GameContext } from '../types'
-
-export const loggingMiddleware: MiddlewareFn<GameContext> = async (ctx, next) => {
+export const loggingMiddleware: BotMiddleware = async (ctx, next) => {
   const startMs = performance.now()
-  const logger = makeLogger(`${ctx.update.update_id}`)
+  const logger = makeLogger(ctx.update.update_id.toString())
 
   ctx.logger = logger
 
