c3909a696bf417f4ee85b9c8da9309f11c994f0d
Max Starikevich <maxim.starikevich@gmail.com>
2023-04-03 14:01:13 +0200

    Improved /register

--- src/bot/actions/register.ts
+++ src/bot/actions/register.ts
@@ -5,25 +5,23 @@ import { Action, ActionHandler, ActionInitializer } from '../types'
 const actionName = /^register-(\d+)$/
 
 const initializer: ActionInitializer = async ctx => {
-  const { game, logger } = ctx
-
-  await game.refreshData({ logger })
+  const { game, lang } = ctx
 
   const players = (await game.getPlayers()).filter(({ isCompanion }) => !isCompanion)
 
   if (players.length === 0) {
-    return await ctx.replyWithHTML('No players.')
+    return await ctx.replyWithHTML(lang.NOT_ENOUGH_PLAYERS_ENROLLED())
   }
 
   const alreadyRegisteredPlayer = players.find(({ telegramUserId }) => telegramUserId !== undefined && telegramUserId === ctx.from.id)
 
   if (alreadyRegisteredPlayer !== undefined) {
-    return await ctx.reply(ctx.lang.REGISTER_ALREADY_REGISTERED())
+    return await ctx.reply(lang.REGISTER_ALREADY_REGISTERED())
   }
 
   const chunkedPlayers = chunk(players, 2)
 
-  await ctx.reply(ctx.lang.REGISTER_CHOOSE_YOURSELF(), {
+  await ctx.reply(lang.REGISTER_CHOOSE_YOURSELF(), {
     reply_markup: {
       inline_keyboard: chunkedPlayers.map(players => [
         ...players.filter(({ telegramUserId }) => telegramUserId === undefined).map(player => ({ text: `${player.name} ${player.clanEmoji ?? ''}`, callback_data: `register-${player.tableRow}` }))
@@ -33,17 +31,17 @@ const initializer: ActionInitializer = async ctx => {
 }
 
 const handler: ActionHandler = async ctx => {
-  const { game } = ctx
+  const { game, lang } = ctx
 
   const tableRow = parseInt(ctx.match[1])
 
   if (Number.isNaN(tableRow)) {
-    throw new Error('Register handler data is wrong.')
+    throw new Error(lang.REGISTER_HANDLER_WRONG_DATA())
   }
 
   const player = await game.registerPlayer(tableRow, ctx.callbackQuery.from.id)
 
-  return await ctx.reply(ctx.lang.REGISTER_SUCCESS({ name: player.name }))
+  return await ctx.reply(lang.REGISTER_SUCCESS({ name: player.name }))
 }
 
 export const register: Action = {
