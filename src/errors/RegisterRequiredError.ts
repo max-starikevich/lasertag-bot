9da51c108ac1283b1f5b7b354aa69b39c1791760
Max Starikevich <maxim.starikevich@gmail.com>
2023-04-07 13:12:44 +0200

    Added new error types

---
+++ src/errors/RegisterRequiredError.ts
@@ -0,0 +1,9 @@
+import { register } from '$/bot/commands/register'
+import { TranslationFunctions } from '$/lang/i18n-types'
+import { CustomError } from './CustomError'
+
+export class RegisterRequiredError extends CustomError {
+  public message = 'The user needs to register first'
+  public replyMessage: (lang: TranslationFunctions) => string = lang => lang.REGISTER_REQUIRED({ registerCommandName: register.name })
+  public shouldBeReported = false
+}
