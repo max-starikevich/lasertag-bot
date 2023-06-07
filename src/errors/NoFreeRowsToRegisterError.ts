364d7caeac4e3dc76a208fb6274ceab3cb078f48
Max Starikevich <maxim.starikevich@gmail.com>
2023-06-08 00:32:48 +0200

    Errors rename

--- src/errors/NoFreeRowsToRegisterError.ts
+++ src/errors/NoFreeRowsToRegisterError.ts
@@ -1,7 +1,7 @@
 import { TranslationFunctions } from '$/lang/i18n-types'
 import { CustomError } from './CustomError'
 
-export class NoFreeRowsToRegister extends CustomError {
+export class NoFreeRowsToRegisterError extends CustomError {
   public message = 'No free rows in the table to register'
   public replyMessage: (lang: TranslationFunctions) => string = lang => lang.REGISTER_NO_FREE_ROWS()
   public shouldBeReported = false
