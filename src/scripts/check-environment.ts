0bcb5a6b75ecf79670dec2965731067961f25830
Max Starikevich <maxim.starikevich@gmail.com>
2023-06-18 13:51:23 +0200

    Better script names

--- src/scripts/check-environment.ts
+++ src/scripts/check-environment.ts
@@ -1,17 +1,14 @@
-/* eslint-disable */
 import 'module-alias/register'
-import dotenv from 'dotenv'
-dotenv.config({ path: '.env.production' })
 
 import { makeLogger } from '$/logger'
 import { checkEnvironment } from '$/config/check'
-/* eslint-enable */
 
 async function run (): Promise<void> {
   const logger = makeLogger()
 
   try {
     await checkEnvironment()
+
     logger.info('✅ Environment is OK')
   } catch (e) {
     logger.error(e)
---
+++ src/scripts/update-webhook.ts
@@ -0,0 +1,25 @@
+import 'module-alias/register'
+
+import { updateBotWebhook } from '$/bot/webhooks'
+import { makeLogger } from '$/logger'
+import { botPromise } from '$/lambda'
+
+async function run (): Promise<void> {
+  const logger = makeLogger()
+
+  try {
+    const bot = await botPromise
+
+    await updateBotWebhook({
+      telegram: bot.telegram,
+      logger
+    })
+
+    logger.info('✅ Updated bot webhook')
+  } catch (e) {
+    logger.error(e)
+    process.exit(1)
+  }
+}
+
+void run()
