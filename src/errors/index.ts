6f640b54c67b7e65b3703f5973a885c41a37a152
Max Starikevich <maxim.starikevich@gmail.com>
2023-06-18 13:46:51 +0200

    Simplified reportException()

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -1,15 +1,5 @@
 import * as Sentry from '@sentry/node'
 
-import config from '$/config'
-import { globalLogger as logger } from '../logger'
-
-export const reportException = (e: any, data?: any): void => {
-  if (config.isLocal) {
-    logger.error(e, data)
-    return
-  }
-
-  Sentry.captureException(e, {
-    extra: { data }
-  })
+export const reportException = (e: any): void => {
+  Sentry.captureException(e)
 }
