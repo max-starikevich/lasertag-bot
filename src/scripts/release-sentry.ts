d69e6472ed6bf38cde4312765f471bbc41b76e8b
Max Starikevich <maxim.starikevich@gmail.com>
2023-06-19 18:46:50 +0200

    New script for Sentry

---
+++ src/scripts/release-sentry.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+import { version } from '../../package.json'
+
+import { makeLogger } from '$/logger'
+
+async function run (): Promise<void> {
+  const logger = makeLogger()
+
+  try {
+    const sentryWebhook = process.env.SENTRY_DEPLOY_WEBHOOK
+
+    if (sentryWebhook === undefined) {
+      throw new Error("Sentry Webhook isn't specified")
+    }
+
+    await axios.post(sentryWebhook, {
+      version
+    })
+
+    logger.info(`✅ Created v${version} release in Sentry`)
+  } catch (e) {
+    logger.error(e)
+    process.exit(1)
+  }
+}
+
+void run()
