8bba4dc5e472d6a0ea2a2a643a73afc4d2afaf93
Max Starikevich <maxim.starikevich@gmail.com>
2023-07-22 11:31:02 +0200

    Renamed commands

--- src/bot/commands/teams.ts
+++ src/bot/commands/teams.ts
@@ -1,10 +1,10 @@
 import { NotEnoughPlayersError } from '$/errors/NotEnoughPlayersError'
-import { getBalancedTeamsWithClans } from '$/game/player/balance/with-clans'
+import { getBalancedTeams } from '$/game/player/balance/no-clans'
 import { getActivePlayers, orderTeamByGameCount } from '$/game/player'
 
 import { Command, CommandHandler } from '../types'
-import { replyWithPlaceAndTime, replyWithSquads, replyWithTeamBalance, replyWithTeamCount } from '.'
-import { initializer as replyWithStatsAction } from '../actions/stats'
+import { replyWithPlaceAndTime, replyWithPlayers, replyWithTeamBalance, replyWithTeamCount } from '.'
+import { initializer as replyWithStatsActions } from '../actions/stats'
 
 const handler: CommandHandler = async (ctx) => {
   await replyWithPlaceAndTime(ctx)
@@ -12,7 +12,7 @@ const handler: CommandHandler = async (ctx) => {
   const { players } = ctx
 
   const activePlayers = getActivePlayers(players)
-  const teams = getBalancedTeamsWithClans(activePlayers)
+  const teams = getBalancedTeams(activePlayers)
   const [redPlayers, bluePlayers] = teams.map(team => orderTeamByGameCount(team))
 
   if (redPlayers.length === 0 || bluePlayers.length === 0) {
@@ -22,20 +22,21 @@ const handler: CommandHandler = async (ctx) => {
   await replyWithTeamCount(ctx, [redPlayers, bluePlayers])
 
   if (redPlayers.length > 0) {
-    await replyWithSquads(ctx, redPlayers, '🔴')
+    await replyWithPlayers(ctx, redPlayers, '🔴')
   }
 
   if (bluePlayers.length > 0) {
-    await replyWithSquads(ctx, bluePlayers, '🔵')
+    await replyWithPlayers(ctx, bluePlayers, '🔵')
   }
 
   await replyWithTeamBalance(ctx, [redPlayers, bluePlayers])
-  await replyWithStatsAction(ctx, teams)
+
+  await replyWithStatsActions(ctx, teams)
 }
 
 export const teams: Command = {
   name: 'teams',
   handler,
-  description: lang => lang.TEAMS_COMMAND_DESCRIPTION(),
+  description: lang => lang.OLD_TEAMS_COMMAND_DESCRIPTION(),
   showInMenu: true
 }
